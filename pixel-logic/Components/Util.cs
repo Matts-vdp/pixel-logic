677de3bb4d1a80020cac1cc80265742505fe2c41
Matts <vdpmatts@gmail.com>
2022-08-06 16:10:50 +0200

    add thread safety

--- pixel-logic/Components/Util.cs
+++ pixel-logic/Components/Util.cs
@@ -19,22 +19,34 @@ namespace Game.Components
 
         public State(int w, int h)
         {
-            _state = new bool[w, h];
+            lock(this) 
+            {
+                _state = new bool[w, h];
+            }
         }
         public void SetState(Pos p, bool newState)
         {
-            _state[p.X, p.Y] = newState;
+            lock(this) 
+            {
+                _state[p.X, p.Y] = newState;
+            }
         }
         public bool GetState(Pos p)
         {
-            return _state[p.X, p.Y];
+            lock(this) 
+            {
+                return _state[p.X, p.Y];
+            }
         }
 
         public void SetText(List<Pos> positions, string text)
         {
-            foreach(Pos p in positions)
+            lock(this) 
             {
-                DrawText[p] = text;
+                foreach(Pos p in positions)
+                {
+                    DrawText[p] = text;
+                }
             }
         }
     }
